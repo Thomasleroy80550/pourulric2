59fb35ab8d5931d1cf013ccef763667a1e38982e
Leroy Thomas <leroythomas@Mac.lan>
2025-07-05 14:59:16 +0200

    [dyad] Agrandissement de la largeur de la page en ajustant les configurations Tailwind CSS. - wrote 2 file(s)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,8 @@ export default {
       center: true,
       padding: "2rem",
       screens: {
-        "2xl": "1400px",
+        "2xl": "1600px", // Increased from 1400px
+        "3xl": "1920px", // Added new breakpoint
       },
     },
     extend: {

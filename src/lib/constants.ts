d8796503f05ce4d1d7d1ae7b60966c2662b259d6
Leroy Thomas <leroythomas@Mac.lan>
2025-08-13 14:53:34 +0200

    [dyad] Correction de l'erreur d'exportation manquante pour CURRENT_CGUV_VERSION - wrote 1 file(s)

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
 export const EMAIL_TEMPLATE_KEY = 'statement_email_template';
 export const CONTACT_EMAIL_KEY = 'contact_email';
 export const CONTACT_PHONE_KEY = 'contact_phone';
+export const CURRENT_CGUV_VERSION = '1.0'; // Ajouté pour résoudre l'erreur d'exportation manquante

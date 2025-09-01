7f765fa6d11ebcb8452cbdcfdc748b3c4a4468e7
Leroy Thomas <leroythomas@Mac.lan>
2025-09-01 14:40:56 +0200

    [dyad] Removing 'use client' directive to fix parsing error - wrote 2 file(s)

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import {
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import MainLayout from "@/components/MainLayout";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";

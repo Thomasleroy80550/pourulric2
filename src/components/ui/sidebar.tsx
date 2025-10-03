69e8c1d19ed39a18c0d997d6e6c1388532ba8c2c
Leroy Thomas <leroythomas@Mac.lan>
2025-10-03 17:01:34 +0200

    [dyad] wrote 6 file(s)

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -379,12 +379,13 @@ const SidebarFooter = React.forwardRef<
     <div
       ref={ref}
       data-sidebar="footer"
-      className={cn("flex flex-col gap-2 p-2", className)}
+      data-slot="footer"
+      className={cn("flex flex-col gap-2 p-4 bg-sidebar", className)}
       {...props}
     />
-  );
-});
-SidebarFooter.displayName = "SidebarFooter";
+  )
+})
+// ... existing code ...
 
 const SidebarSeparator = React.forwardRef<
   React.ElementRef<typeof Separator>,

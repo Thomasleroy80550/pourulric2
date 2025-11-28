bdcd7ce4908cbe230127d66a958bbb9f7ec5edd4
Thomasleroy80550 <thomasleroy80550@gmail.com>
2025-11-28 19:41:44 +0100

    [dyad] Correction de l’ordre de déclaration de chartView - wrote 1 file(s)

--- src/pages/ElectricityConsumptionPage.tsx
+++ src/pages/ElectricityConsumptionPage.tsx
@@ -787,8 +787,8 @@ const ElectricityConsumptionPage: React.FC = () => {
 
   const [showDebug, setShowDebug] = React.useState(false);
   const [debugInfo, setDebugInfo] = React.useState<any>(null);
-  // Vue du graphique: 'area' ou 'bars'
   const [chartView, setChartView] = React.useState<"area" | "bars">("bars");
+
   // Limite au-delà de laquelle la vue colonnes devient illisible
   const barsPointLimit = 220;
 
